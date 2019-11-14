2c3af2e43a367713c0b2a765fe2ddafcd4910ff6
Sam <Jeffcoat>
2019-11-14 15:46:29 -0600

    Adds styles to navBar and renders with on click, also adds initial state

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,13 +1,33 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  //adding state to our class component with our constructor
+  constructor(props) {
+    super(props)
+    this.state = {
+      selectedLanguage : 'All'
+    }
+    // putting this here now when we pass it down to another component.
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+  updateLanguage (selectedLanguage) {
+    this.setState({
+      selectedLanguage
+    })
+  }
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'Python']
     return (
       <ul className = 'flex-center'>
       {languages.map((language) => (
         <li key = {language}>
-        <button className = 'btn-clear nav-link'>
+        <button
+        className = 'btn-clear nav-link'
+
+        style = {language === this.state.selectedLanguage ? { color : 'rgb(187, 46, 31)' } : null}
+        onClick = {() => this.updateLanguage(language)}
+        >
         {language}
         </button>
         </li>
