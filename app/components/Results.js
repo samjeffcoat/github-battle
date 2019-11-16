b92e5afa690eb6eb81bc29d513b0c17340957c44
Sam <Jeffcoat>
2019-11-15 21:59:53 -0600

    Results Component skeleton is built out and is rendering successfully

---
+++ app/components/Results.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Results extends React.Component {
+  render() {
+    return (
+      <div>
+        Results
+        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+      </div>
+    );
+  }
+}
