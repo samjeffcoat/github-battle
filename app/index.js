1314186841d11dd052dc59ed2bee865ad15ace4e
Sam <Jeffcoat>
2019-11-19 12:25:37 -0600

    Toggle dark theme passed to battle component

--- app/index.js
+++ app/index.js
@@ -1,41 +1,37 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import Popular from './components/Popular'
-import Battle from './components/Battle'
-import { ThemeProvider } from './contexts/theme'
-import Nav from './components/Nav'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Popular from "./components/Popular";
+import Battle from "./components/Battle";
+import { ThemeProvider } from "./contexts/theme";
+import Nav from "./components/Nav";
 
 class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      theme: 'light',
+      theme: "light",
       toggleTheme: () => {
         this.setState(({ theme }) => ({
-          theme: theme === 'light' ? 'dark' : 'light'
-        }))
+          theme: theme === "light" ? "dark" : "light"
+        }));
       }
-    }
+    };
   }
   render() {
     return (
       <ThemeProvider value={this.state}>
         <div className={this.state.theme}>
-          <div className='container'>
+          <div className="container">
             <Nav />
 
-            <Battle />
+            <Popular />
           </div>
         </div>
       </ThemeProvider>
-    )
+    );
   }
 }
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+ReactDOM.render(<App />, document.getElementById("app"));
