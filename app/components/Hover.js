922390ebdab697bd481ee33febb2ff6da375a20a
Sam <Jeffcoat>
2019-11-19 17:20:23 -0600

    Adds query strings into our components for better navigation

--- app/components/Hover.js
+++ app/components/Hover.js
@@ -1,32 +1,31 @@
-
-import React from 'react'
+import React from "react";
 
 export default class Hover extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
-      hovering: false,
-    }
+      hovering: false
+    };
 
-    this.mouseOver = this.mouseOver.bind(this)
-    this.mouseOut = this.mouseOut.bind(this)
+    this.mouseOver = this.mouseOver.bind(this);
+    this.mouseOut = this.mouseOut.bind(this);
   }
   mouseOver() {
     this.setState({
       hovering: true
-    })
+    });
   }
   mouseOut() {
     this.setState({
       hovering: false
-    })
+    });
   }
-  render () {
+  render() {
     return (
       <div onMouseOver={this.mouseOver} onMouseOut={this.mouseOut}>
         {this.props.children(this.state.hovering)}
       </div>
-    )
+    );
   }
 }
